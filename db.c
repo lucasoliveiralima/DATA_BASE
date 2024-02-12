6f4da6ef2f38b459e739672d294fd969d03f9d59
Lucas Oliveira Lima <lucasoliveirallima@gmail.com>
2024-02-12 10:40:11 -0300

    added functionality .exit

--- db.c
+++ db.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef struct {
     char* buffer; 
@@ -9,142 +10,48 @@ typedef struct {
     ssize_t input_length; 
 } InputBuffer; 
 
-typedef enum 
-{
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
-
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
-typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
-typedef struct 
-{
-    uint32_t id; 
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
-} Row;
-
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNEMA_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USENAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
-typedef struct 
-{
-    StatementType type;
-    Row row_to_insert; // only used by insert statement
-} Statement;
-
-InputBuffer* new_input_buffer()
-{
+InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0; 
-    input_buffer->input_length = 0;
+    input_buffer->input_length = 0; 
 
     return input_buffer;
 }
 
-void print_prompt() { printf("db > "); }
+void print_prompt() {printf("db > "); }
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-
-void read_input(InputBuffer* input_buffer)
-{
+void read_input(InputBuffer* input_buffer) {
     ssize_t bytes_read = 
-        getline(&(input_buffer->buffer), &(input_beffer->buffer_length), stdin);
-
+        getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
     if (bytes_read <= 0) {
         printf("Error reading input\n");
         exit(EXIT_FAILURE);
     }
 
-    // Ignore trailing newline 
+    // Ignore trailing newline
     input_buffer->input_length = bytes_read - 1; 
-    input_buffer->buffer[bytes_read -1] = 0;
+    input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer) 
-{
+void close_input_buffer(InputBuffer* input_buffer){ 
     free(input_buffer->buffer);
     free(input_buffer);
 }
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) 
-{
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        exit(EXIT_SUCCESS);
-    } else {
-        return META_COMMAND_UNRECOGNIZED_COMMAND;
-    }
-}
 
-PrepareResult prepare_statement (InputBuffer* input_buffer, Statement* statement) 
-{
-    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-                input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-                statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        return PREPARE_SUCCESS;
-    }
-    if (strcmp(input_buffer->buffer, "select") == 0) {
-        statement->type = STATEMENT_SELECT; 
-        return PREPARE_SUCCESS;
-    }
-
-    return PREPARE_UNRECOGNIZED_STATEMENT;
-}
-
-void execute_statement(Statement* statement) 
-{
-    switch (statement->type) { 
-        case (STATEMENT_INSERT): 
-            printf("This is where we would do an insert.\n");
-            break;
-        case (STATEMENT_SELECT):
-            printf("This is where we would do a select.\n");
-            break;
-    }
-}
-
-int main(int args, char* argv[]) 
-{
+int main(int argc, char** argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
-        read_input(input_buffer); 
-    
-    if (input_buffer->buffer[0] == '.') {
-        switch (do_meta_command(input_buffer)) {
-            case (META_COMMAND_SUCCESS):
-                continue; 
-            case (META_COMMAND_UNRECOGNIZED_COMMAND):
-                printf("Uncognized command '%s'\n.", input_buffer->buffer);
-                continue;
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
         }
     }
-    
-    Statement statement; 
-    switch (prepare_statement(input_buffer, &statement)) {
-        case (PREPARE_SUCCESS):
-            break;
-        case (PREPARE_UNRECOGNIZED_STATEMENT):
-            printf("Unrecognized keyword at start of '%s'.\n",
-                    input_buffer->buffer);
-            continue;
-    }
-
-    execute_statement(&statement);
-    printf("Executed.\n");
-    }
 }
+
